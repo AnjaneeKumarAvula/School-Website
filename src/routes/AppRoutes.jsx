a6b88d042f299cce9ccb90bc6e37f38633a85a1f
Anjanee Kumar Avula <anjanikumaravula@gmail.com>
2026-04-22 18:06:21 +0530

    Add files via upload

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home.jsx";
+import ParentLogin from "../pages/ParentLogin.jsx";
+import ParentDashboard from "../pages/ParentDashboard.jsx";
+import TeacherLogin from "../pages/TeacherLogin.jsx";
+import TeacherDashboard from "../pages/TeacherDashboard.jsx";
+import AdminDashboard from "../pages/AdminDashboard.jsx";
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        {/* Home page - first page */}
+        <Route path="/" element={<Home />} />
+
+        {/* Parent */}
+        <Route path="/parent/login" element={<ParentLogin />} />
+        <Route path="/parent/dashboard" element={<ParentDashboard />} />
+
+        {/* Teacher */}
+        <Route path="/teacher/login" element={<TeacherLogin />} />
+        <Route path="/teacher/dashboard" element={<TeacherDashboard />} />
+
+        {/* Admin */}
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        {/* 404 */}
+        <Route path="*" element={<Home />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
